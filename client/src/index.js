759c8f12584bcb8e4412a262182f19f8da3bf026
srmchartroom <sean@chartroomcreative.com>
2020-10-13 20:26:45 -0400

    overwrite all previous files with new base app for SERN (mysql, express, react, node) plus passport auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+import { StoreProvider } from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <StoreProvider>
+        <App />
+      </StoreProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
